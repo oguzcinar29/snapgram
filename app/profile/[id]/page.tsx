9b5252d65caa20295f2126cc7a0148940b71ee84
Oguz Cinar <marazoguz_29@hotmail.com>
2024-03-29 15:30:38 +0300

    last last push

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,6 +1,8 @@
 import SingleUser from "@/components/shared/SingleUser";
 import { getAllPosts } from "@/lib/actions/post.actions";
 import { getAllUsers, getUserById } from "@/lib/actions/user.actions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 export const dynamic = "force-dynamic";
 export default async function UserProfile({ params }: any) {
@@ -8,7 +10,10 @@ export default async function UserProfile({ params }: any) {
   const posts = await getAllPosts();
   const users = await getAllUsers();
   const user = await getUserById({ id });
-
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/sign-in");
+  }
   return (
     <div>
       <SingleUser posts={posts} users={users} userId={id} {...user} />
